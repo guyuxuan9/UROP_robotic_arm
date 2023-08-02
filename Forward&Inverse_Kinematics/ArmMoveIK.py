d2b0ae69f9a8093b52ffef84b6b6f32f451c11e7
guyuxuan9 <guyuxuan9@gmail.com>
2023-08-02 19:43:58 +0800

    update inverse kinematics files

--- Forward&Inverse_Kinematics/ArmMoveIK.py
+++ Forward&Inverse_Kinematics/ArmMoveIK.py
@@ -1,25 +1,21 @@
-#!/usr/bin/env python3
-# encoding:utf-8
-import sys
-# sys.path.append('/home/ubuntu/Sensor/')
 import time
 import numpy as np
-from math import sqrt
 #import matplotlib.pyplot as plt
 from InverseKinematics import *
-from Transform import getAngle
-#from mpl_toolkits.mplot3d import Axes3D
-from Board import setBusServoPulse, getBusServoPulse
+from Board import setBusServoPulse
 
-#机械臂根据逆运动学算出的角度进行移动
-ik = IK('arm')
-#设置连杆长度
+# Initialse the InverseKinematics class
+ik = IK()
+
+# Set the length of the link
 l1 = ik.l1 + 0.75
 l4 = ik.l4 - 0.15
 ik.setLinkLength(L1=l1, L4=l4)
 
 class ArmIK:
-    servo3Range = (0, 1000.0, 0, 240.0) #脉宽， 角度
+    # According to the datasheet, each bus servo motor can only rotate between 0 and 240 degrees and the coresponding pulse widths are 0 and 1000
+    
+    servo3Range = (0, 1000.0, 0, 240.0) # (pulse width min， pulse width max, angle min, angle max)
     servo4Range = (0, 1000.0, 0, 240.0)
     servo5Range = (0, 1000.0, 0, 240.0)
     servo6Range = (0, 1000.0, 0, 240.0)
@@ -28,31 +24,34 @@ class ArmIK:
         self.setServoRange()
 
     def setServoRange(self, servo3_Range=servo3Range, servo4_Range=servo4Range, servo5_Range=servo5Range, servo6_Range=servo6Range):
-        # 适配不同的舵机
+        
         self.servo3Range = servo3_Range
         self.servo4Range = servo4_Range
         self.servo5Range = servo5_Range
         self.servo6Range = servo6_Range
+        
+        # Param unit: pulse_width / degree
         self.servo3Param = (self.servo3Range[1] - self.servo3Range[0]) / (self.servo3Range[3] - self.servo3Range[2])
         self.servo4Param = (self.servo4Range[1] - self.servo4Range[0]) / (self.servo4Range[3] - self.servo4Range[2])
         self.servo5Param = (self.servo5Range[1] - self.servo5Range[0]) / (self.servo5Range[3] - self.servo5Range[2])
         self.servo6Param = (self.servo6Range[1] - self.servo6Range[0]) / (self.servo6Range[3] - self.servo6Range[2])
 
     def transformAngelAdaptArm(self, theta3, theta4, theta5, theta6):
-        #将逆运动学算出的角度转换为舵机对应的脉宽值
+        # transform the angle calcualted from inverse kinematics to pulse widths
         servo3 = int(round(theta3 * self.servo3Param + (self.servo3Range[1] + self.servo3Range[0])/2))
         if servo3 > self.servo3Range[1] or servo3 < self.servo3Range[0] + 60:
-            logger.info('servo3(%s)超出范围(%s, %s)', servo3, self.servo3Range[0] + 60, self.servo3Range[1])
+            logger.info('servo3(%s) out of range (%s, %s)', servo3, self.servo3Range[0] + 60, self.servo3Range[1])
             return False
 
         servo4 = int(round(theta4 * self.servo4Param + (self.servo4Range[1] + self.servo4Range[0])/2))
         if servo4 > self.servo4Range[1] or servo4 < self.servo4Range[0]:
-            logger.info('servo4(%s)超出范围(%s, %s)', servo4, self.servo4Range[0], self.servo4Range[1])
+            logger.info('servo4(%s) out of range (%s, %s)', servo4, self.servo4Range[0], self.servo4Range[1])
             return False
 
         servo5 = int(round((self.servo5Range[1] + self.servo5Range[0])/2 - (90.0 - theta5) * self.servo5Param))
+        # When theta5=0 or 180, the link l2 in purely horizontal. It cannot go beyong this value. Otherwise, the robot will be damaged.
         if servo5 > ((self.servo5Range[1] + self.servo5Range[0])/2 + 90*self.servo5Param) or servo5 < ((self.servo5Range[1] + self.servo5Range[0])/2 - 90*self.servo5Param):
-            logger.info('servo5(%s)超出范围(%s, %s)', servo5, self.servo5Range[0], self.servo5Range[1])
+            logger.info('servo5(%s) out of range (%s, %s)', servo5, self.servo5Range[0], self.servo5Range[1])
             return False
         
         if theta6 < -(self.servo6Range[3] - self.servo6Range[2])/2:
@@ -60,37 +59,34 @@ class ArmIK:
         else:
             servo6 = int(round(((self.servo6Range[3] - self.servo6Range[2])/2 - (90 - theta6)) * self.servo6Param))
         if servo6 > self.servo6Range[1] or servo6 < self.servo6Range[0]:
-            logger.info('servo6(%s)超出范围(%s, %s)', servo6, self.servo6Range[0], self.servo6Range[1])
+            logger.info('servo6(%s) out of range (%s, %s)', servo6, self.servo6Range[0], self.servo6Range[1])
             return False
 
         return {"servo3": servo3, "servo4": servo4, "servo5": servo5, "servo6": servo6}
 
-    def servosMove(self, servos, movetime=None):
-        #驱动3,4,5,6号舵机转动
+    def servosMove(self, servos, movetime):
+        """ Drive servo motor 3,4,5,6"""
         time.sleep(0.02)
-        if movetime is None:
-            max_d = 0
-            for i in  range(0, 4):
-                d = abs(getBusServoPulse(i + 3) - servos[i])
-                if d > max_d:
-                    max_d = d
-            movetime = int(max_d*4)
         setBusServoPulse(3, servos[0], movetime)
         setBusServoPulse(4, servos[1], movetime)
         setBusServoPulse(5, servos[2], movetime)
         setBusServoPulse(6, servos[3], movetime)
 
-        return movetime
 
     def setPitchRange(self, coordinate_data, alpha1, alpha2, da = 1):
-        #给定坐标coordinate_data和俯仰角的范围alpha1，alpha2, 自动在范围内寻找到的合适的解
-        #如果无解返回False,否则返回对应舵机角度,俯仰角
-        #坐标单位cm， 以元组形式传入，例如(0, 5, 10)
-        #da为俯仰角遍历时每次增加的角度
+        """Given the coordinate_data and the range of pitch angle (alpha1,alpha2), find the solution of inverse kinematics.
+           If there are no solutions, return False. Otherwise, return the rotation angle of each servo motor and the pitch angle
+           
+           Input:
+                coordinate_data (tuple): coordinates of the end-effector in cm. E.g.  (0,15,15)
+                alpha1: upper/lower limit of pitch angle
+                alpha2: lower/upper limit of pitch angle
+                da: increment in the for loop"""
+                
         x, y, z = coordinate_data
         if alpha1 >= alpha2:
             da = -da
-        for alpha in np.arange(alpha1, alpha2, da):#遍历求解
+        for alpha in np.arange(alpha1, alpha2, da):
             result = ik.getRotationAngle((x, y, z), alpha)
             if result:
                 theta3, theta4, theta5, theta6 = result['theta3'], result['theta4'], result['theta5'], result['theta6']
@@ -100,13 +96,15 @@ class ArmIK:
 
         return False
 
-    def setPitchRangeMoving(self, coordinate_data, alpha, alpha1, alpha2, movetime=None):
-        #给定坐标coordinate_data和俯仰角alpha,以及俯仰角范围的范围alpha1, alpha2，自动寻找最接近给定俯仰角的解，并转到目标位置
-        #如果无解返回False,否则返回舵机角度、俯仰角、运行时间
-        #坐标单位cm， 以元组形式传入，例如(0, 5, 10)
-        #alpha为给定俯仰角
-        #alpha1和alpha2为俯仰角的取值范围
-        #movetime为舵机转动时间，单位ms, 如果不给出时间，则自动计算
+    def setPitchRangeMoving(self, coordinate_data, alpha, alpha1, alpha2, movetime):
+        """Given the coodinates, pitch angle alpha, and the range of the pitch angle (alpha1, alpha2), it will find the solution closest to the given alpha within the range and move the servo motor to that position
+        
+        Input:
+            coordinate_data (tuple): coordinates of the end-effector in cm. E.g.  (0,15,15)
+            alpha (degrees): expected pitch angle
+            alpha1 (degrees): upper/lower limit of pitch angle
+            alpha2 (degrees): lower/upper limit of pitch angle
+            movetime (ms): duration"""
         x, y, z = coordinate_data
         result1 = self.setPitchRange((x, y, z), alpha, alpha1)
         result2 = self.setPitchRange((x, y, z), alpha, alpha2)
@@ -122,50 +120,10 @@ class ArmIK:
                 return False
         servos, alpha = data[0], data[1]
 
-        movetime = self.servosMove((servos["servo3"], servos["servo4"], servos["servo5"], servos["servo6"]), movetime)
+        self.servosMove((servos["servo3"], servos["servo4"], servos["servo5"], servos["servo6"]), movetime)
 
         return servos, alpha, movetime
-    '''
-    #for test
-    def drawMoveRange2D(self, x_min, x_max, dx, y_min, y_max, dy, z, a_min, a_max, da):
-        # 测试可到达点, 以2d图形式展现，z固定
-        #测试可到达点, 以3d图形式展现，如果点过多，3d图会比较难旋转
-        try:
-            for y in np.arange(y_min, y_max, dy):
-                for x in np.arange(x_min, x_max, dx):
-                    result = self.setPitchRange((x, y, z), a_min, a_max, da)
-                    if result:
-                        plt.scatter(x, y, s=np.pi, c='r')
 
-            plt.xlabel('X Label')
-            plt.ylabel('Y Label')
-
-            plt.show()
-        except Exception as e:
-            print(e)
-            pass
-
-    def drawMoveRange3D(self, x_min, x_max, dx, y_min, y_max, dy, z_min, z_max, dz, a_min, a_max, da):
-        #测试可到达点, 以3d图形式展现，如果点过多，3d图会比较难旋转
-        fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
-        try:
-            for z in np.arange(z_min, z_max, dz):
-                for y in np.arange(y_min, y_max, dy):
-                    for x in np.arange(x_min, x_max, dx):
-                        result = self.setPitchRange((x, y, z), a_min, a_max, da)
-                        if result:
-                            ax.scatter(x, y, z, s=np.pi, c='r')
-
-            ax.set_xlabel('X Label')
-            ax.set_ylabel('Y Label')
-            ax.set_zlabel('Z Label')
-
-            plt.show()
-        except Exception as e:
-            print(e)
-            pass
-    '''
 
 if __name__ == "__main__":
     AK = ArmIK()
@@ -174,4 +132,3 @@ if __name__ == "__main__":
     #AK.setPitchRangeMoving((0, 10, 10), -30, -90, 0, 2000)
     #time.sleep(2)
     print(AK.setPitchRangeMoving((-4.8, 15, 1.5), 0, -90, 0, 2000))
-    #AK.drawMoveRange2D(-10, 10, 0.2, 10, 30, 0.2, 2.5, -90, 90, 1)
--- Forward&Inverse_Kinematics/InverseKinematics.py
+++ Forward&Inverse_Kinematics/InverseKinematics.py
@@ -1,127 +1,4 @@
-# #!/usr/bin/env python3
-# # encoding: utf-8
-# # 4自由度机械臂逆运动学：给定相应的坐标（X,Y,Z），以及俯仰角，计算出每个关节转动的角度
-# # 2020/07/20 Aiden
-# import logging
-# from math import *
-
-# # CRITICAL, ERROR, WARNING, INFO, DEBUG
-# logging.basicConfig(level=logging.ERROR)
-# logger = logging.getLogger(__name__)
-
-# class IK:
-#     # 舵机从下往上数
-#     # 公用参数，即4自由度机械臂的连杆参数
-#     l1 = 6.10    #机械臂底盘中心到第二个舵机中心轴的距离6.10cm
-#     l2 = 10.16   #第二个舵机到第三个舵机的距离10.16cm
-#     l3 = 9.64    #第三个舵机到第四个舵机的距离9.64cm
-#     l4 = 0.00    #这里不做具体赋值，根据初始化时的选择进行重新赋值
-
-#     # 气泵款特有参数
-#     l5 = 4.70  #第四个舵机到吸嘴正上方的距离4.70cm
-#     l6 = 4.46  #吸嘴正上方到吸嘴的距离4.46cm
-#     alpha = degrees(atan(l6 / l5))  #计算l5和l4的夹角
-
-#     def __init__(self, arm_type): #根据不同款的夹持器，适配参数
-#         self.arm_type = arm_type
-#         if self.arm_type == 'pump': #如果是气泵款机械臂
-#             self.l4 = sqrt(pow(self.l5, 2) + pow(self.l6, 2))  #第四个舵机到吸嘴作为第四个连杆
-#         elif self.arm_type == 'arm':
-#             self.l4 = 16.65  #第四个舵机到机械臂末端的距离16.6cm， 机械臂末端是指爪子完全闭合时
-
-#     def setLinkLength(self, L1=l1, L2=l2, L3=l3, L4=l4, L5=l5, L6=l6):
-#         # 更改机械臂的连杆长度，为了适配相同结构不同长度的机械臂
-#         self.l1 = L1
-#         self.l2 = L2
-#         self.l3 = L3
-#         self.l4 = L4
-#         self.l5 = L5
-#         self.l6 = L6
-#         if self.arm_type == 'pump':
-#             self.l4 = sqrt(pow(self.l5, 2) + pow(self.l6, 2))
-#             self.alpha = degrees(atan(self.l6 / self.l5))
-
-#     def getLinkLength(self):
-#         # 获取当前设置的连杆长度
-#         if self.arm_type == 'pump':
-#             return {"L1":self.l1, "L2":self.l2, "L3":self.l3, "L4":self.l4, "L5":self.l5, "L6":self.l6}
-#         else:
-#             return {"L1":self.l1, "L2":self.l2, "L3":self.l3, "L4":self.l4}
-
-#     def getRotationAngle(self, coordinate_data, Alpha):
-#         # 给定指定坐标和俯仰角，返回每个关节应该旋转的角度，如果无解返回False
-#         # coordinate_data为夹持器末端坐标，坐标单位cm， 以元组形式传入，例如(0, 5, 10)
-#         # Alpha为夹持器与水平面的夹角，单位度
-
-#         # 设夹持器末端为P(X, Y, Z), 坐标原点为O, 原点为云台中心在地面的投影， P点在地面的投影为P_
-#         # l1与l2的交点为A, l2与l3的交点为B，l3与l4的交点为C
-#         # CD与PD垂直，CD与z轴垂直，则俯仰角Alpha为DC与PC的夹角, AE垂直DP_， 且E在DP_上， CF垂直AE，且F在AE上
-#         # 夹角表示：例如AB和BC的夹角表示为ABC
-#         X, Y, Z = coordinate_data
-#         if self.arm_type == 'pump':
-#             Alpha -= self.alpha
-#         #求底座旋转角度
-#         theta6 = degrees(atan2(Y, X))
- 
-#         P_O = sqrt(X*X + Y*Y) #P_到原点O距离
-#         CD = self.l4 * cos(radians(Alpha))
-#         PD = self.l4 * sin(radians(Alpha)) #当俯仰角为正时，PD为正，当俯仰角为负时，PD为负
-#         AF = P_O - CD
-#         CF = Z - self.l1 - PD
-#         AC = sqrt(pow(AF, 2) + pow(CF, 2))
-#         if round(CF, 4) < -self.l1:
-#             logger.debug('高度低于0, CF(%s)<l1(%s)', CF, -self.l1)
-#             return False
-#         if self.l2 + self.l3 < round(AC, 4): #两边之和小于第三边
-#             logger.debug('不能构成连杆结构, l2(%s) + l3(%s) < AC(%s)', self.l2, self.l3, AC)
-#             return False
-
-#         #求theat4
-#         cos_ABC = round(-(pow(AC, 2)- pow(self.l2, 2) - pow(self.l3, 2))/(2*self.l2*self.l3), 4) #余弦定理
-#         if abs(cos_ABC) > 1:
-#             logger.debug('不能构成连杆结构, abs(cos_ABC(%s)) > 1', cos_ABC)
-#             return False
-#         ABC = acos(cos_ABC) #反三角算出弧度
-#         theta4 = 180.0 - degrees(ABC)
-
-#         #求theta5
-#         CAF = acos(AF / AC)
-#         cos_BAC = round((pow(AC, 2) + pow(self.l2, 2) - pow(self.l3, 2))/(2*self.l2*AC), 4) #余弦定理
-#         if abs(cos_BAC) > 1:
-#             logger.debug('不能构成连杆结构, abs(cos_BAC(%s)) > 1', cos_BAC)
-#             return False
-#         if CF < 0:
-#             zf_flag = -1
-#         else:
-#             zf_flag = 1
-#         theta5 = degrees(CAF * zf_flag + acos(cos_BAC))
-
-#         #求theta3
-#         theta3 = Alpha - theta5 + theta4
-#         if self.arm_type == 'pump':
-#             theta3 += self.alpha
-
-#         return {"theta3":theta3, "theta4":theta4, "theta5":theta5, "theta6":theta6} # 有解时返回角度字典
-            
-# if __name__ == '__main__':
-#     ik = IK('arm')
-#     ik.setLinkLength(L1=ik.l1 + 0.89, L4=ik.l4 - 0.3)
-#     print('连杆长度：', ik.getLinkLength())
-#     print(ik.getRotationAngle((0, 0, ik.l1 + ik.l2 + ik.l3 + ik.l4), 90))
-
-
-
-
-
-
-
-
-
-
-# !/usr/bin/env python3
-# encoding: utf-8
-# 4自由度机械臂逆运动学：给定相应的坐标（X,Y,Z），以及俯仰角，计算出每个关节转动的角度
-# 2020/07/20 Aiden
+# 4DOF robotic arm inverse kinematics 
 import logging
 from math import *
 
@@ -130,89 +7,80 @@ logging.basicConfig(level=logging.ERROR)
 logger = logging.getLogger(__name__)
 
 class IK:
-    # 舵机从下往上数
-    # 公用参数，即4自由度机械臂的连杆参数
-    l1 = 6.10    #机械臂底盘中心到第二个舵机中心轴的距离6.10cm
-    l2 = 10.16   #第二个舵机到第三个舵机的距离10.16cm
-    l3 = 9.64    #第三个舵机到第四个舵机的距离9.64cm
-    l4 = 0.00    #这里不做具体赋值，根据初始化时的选择进行重新赋值
+    # servo motor ID is counted from the bottom
+    # length of each link
+    l1 = 6.10    # distance from the centre of the base to the second motor
+    l2 = 10.16   # distance from the second motor to the third one
+    l3 = 9.64    # distance from the third motor to the fourth one
+    l4 = 16.65   # distance from the fourth motor to the end effector
 
-    # 气泵款特有参数
-    l5 = 4.70  #第四个舵机到吸嘴正上方的距离4.70cm
-    l6 = 4.46  #吸嘴正上方到吸嘴的距离4.46cm
-    alpha = degrees(atan(l6 / l5))  #计算l5和l4的夹角
 
-    def __init__(self, arm_type): #根据不同款的夹持器，适配参数
-        self.arm_type = arm_type
-        if self.arm_type == 'pump': #如果是气泵款机械臂
-            self.l4 = sqrt(pow(self.l5, 2) + pow(self.l6, 2))  #第四个舵机到吸嘴作为第四个连杆
-        elif self.arm_type == 'arm':
-            self.l4 = 16.65  #第四个舵机到机械臂末端的距离16.6cm， 机械臂末端是指爪子完全闭合时
+    def __init__(self): 
+        print("Initialise an InverseKinematics class.")
 
-    def setLinkLength(self, L1=l1, L2=l2, L3=l3, L4=l4, L5=l5, L6=l6):
-        # 更改机械臂的连杆长度，为了适配相同结构不同长度的机械臂
+    def setLinkLength(self, L1=l1, L2=l2, L3=l3, L4=l4):
+        # set the length mannually
         self.l1 = L1
         self.l2 = L2
         self.l3 = L3
         self.l4 = L4
-        self.l5 = L5
-        self.l6 = L6
-        if self.arm_type == 'pump':
-            self.l4 = sqrt(pow(self.l5, 2) + pow(self.l6, 2))
-            self.alpha = degrees(atan(self.l6 / self.l5))
 
-    def getLinkLength(self):
-        # 获取当前设置的连杆长度
-        if self.arm_type == 'pump':
-            return {"L1":self.l1, "L2":self.l2, "L3":self.l3, "L4":self.l4, "L5":self.l5, "L6":self.l6}
-        else:
-            return {"L1":self.l1, "L2":self.l2, "L3":self.l3, "L4":self.l4}
+    def getLinkLength(self):  
+        return {"L1":self.l1, "L2":self.l2, "L3":self.l3, "L4":self.l4}
 
-    def getRotationAngle(self, coordinate_data, Alpha):
-        # 给定指定坐标和俯仰角，返回每个关节应该旋转的角度，如果无解返回False
-        # coordinate_data为夹持器末端坐标，坐标单位cm， 以元组形式传入，例如(0, 5, 10)
-        # Alpha为夹持器与水平面的夹角，单位度
+    def getRotationAngle(self, coordinate_data, Alpha):        
+        """This function takes in the expected coordinates and pitch angle and calculates the rotation angle of each joint based on the inverse kinematics
+        
+        Inputs:
+            coodinates_data (tuple): coordinates of the end-effector in cm. E.g. (0,15,15)
+            Alpha: pitch angle of the end-effector in degrees
 
-        # 设夹持器末端为P(X, Y, Z), 坐标原点为O, 原点为云台中心在地面的投影， P点在地面的投影为P_
-        # l1与l2的交点为A, l2与l3的交点为B，l3与l4的交点为C
-        # CD与PD垂直，CD与z轴垂直，则俯仰角Alpha为DC与PC的夹角, AE垂直DP_， 且E在DP_上， CF垂直AE，且F在AE上
-        # 夹角表示：例如AB和BC的夹角表示为ABC
+        Returns:
+            __dictionary__: rotation angle of each joint: {"theta3":theta3, "theta4":theta4, "theta5":theta5, "theta6":theta6} 
+            __bool__: False (if there is no solution)
+        """
+        
+        # Let the coodinates of the end-effector be P(x_p, y_p, z_p) and the origin be O. P' is the projection of P on the x-y plane
+        # Let the intersection point of l1 and l2 be A, 12 and l3 be B, l3 and l4 be C
+        # CD is perpendicular to PD. CD is perpendicular to z-axis. Alpha is the angle between DC and PC
+        # AE is perpendicular to DP' and E is on DP'
+        # CF is perpendicular to AE and F is on AE
+
         X, Y, Z = coordinate_data
-        if self.arm_type == 'pump':
-            Alpha -= self.alpha
-        #求底座旋转角度
+
+        # Find the rotation angle of the base
         theta6 = degrees(atan2(Y, X))
  
-        P_O = sqrt(X*X + Y*Y) #P_到原点O距离
+        P_O = sqrt(X*X + Y*Y) # distance from P' to the origin O
         CD = self.l4 * cos(radians(Alpha))
-        PD = self.l4 * sin(radians(Alpha)) #当俯仰角为正时，PD为正，当俯仰角为负时，PD为负
+        PD = self.l4 * sin(radians(Alpha)) # When the pitch angle is positive, PD is positive and vice versa
         AF = P_O - CD
         CF = Z - self.l1 - PD
         AC = sqrt(pow(AF, 2) + pow(CF, 2))
         
         
-        print("P_O: ",P_O, "\nCD: ",CD,"\nPD: ",PD, "\nAF: ",AF,"\nCF: ",CF)
-        print("AC: ",AC)
-        if round(CF, 4) < -self.l1:
-            logger.debug('高度低于0, CF(%s)<l1(%s)', CF, -self.l1)
+        # print("P_O: ",P_O, "\nCD: ",CD,"\nPD: ",PD, "\nAF: ",AF,"\nCF: ",CF)
+        # print("AC: ",AC)
+        if round(CF, 4) < -self.l1: # point C will be below ground level
+            logger.debug('Height is less than 0, CF(%s)<l1(%s)', CF, -self.l1)
             return False
-        if self.l2 + self.l3 < round(AC, 4): #两边之和小于第三边
-            logger.debug('不能构成连杆结构, l2(%s) + l3(%s) < AC(%s)', self.l2, self.l3, AC)
+        if self.l2 + self.l3 < round(AC, 4): # In triangle ABC, the sum of two sides is less than the third side
+            logger.debug('Cannot form a triangle, l2(%s) + l3(%s) < AC(%s)', self.l2, self.l3, AC)
             return False
 
-        #求theat4
-        cos_ABC = round(-(pow(AC, 2)- pow(self.l2, 2) - pow(self.l3, 2))/(2*self.l2*self.l3), 4) #余弦定理
+        # Find theat4
+        cos_ABC = round(-(pow(AC, 2)- pow(self.l2, 2) - pow(self.l3, 2))/(2*self.l2*self.l3), 4) # cosine rule
         if abs(cos_ABC) > 1:
-            logger.debug('不能构成连杆结构, abs(cos_ABC(%s)) > 1', cos_ABC)
+            logger.debug('Angle ABC does not exist, abs(cos_ABC(%s)) > 1', cos_ABC)
             return False
-        ABC = acos(cos_ABC) #反三角算出弧度
+        ABC = acos(cos_ABC)
         theta4 = 180.0 - degrees(ABC)
 
-        #求theta5
+        # Find theta5
         CAF = acos(AF / AC)
-        cos_BAC = round((pow(AC, 2) + pow(self.l2, 2) - pow(self.l3, 2))/(2*self.l2*AC), 4) #余弦定理
+        cos_BAC = round((pow(AC, 2) + pow(self.l2, 2) - pow(self.l3, 2))/(2*self.l2*AC), 4) # cosine rule
         if abs(cos_BAC) > 1:
-            logger.debug('不能构成连杆结构, abs(cos_BAC(%s)) > 1', cos_BAC)
+            logger.debug('Angle BAC does not exist, abs(cos_BAC(%s)) > 1', cos_BAC)
             return False
         if CF < 0:
             zf_flag = -1
@@ -220,16 +88,14 @@ class IK:
             zf_flag = 1
         theta5 = degrees(CAF * zf_flag + acos(cos_BAC))
 
-        #求theta3
+        # Find theta3
         theta3 = Alpha - theta5 + theta4
-        if self.arm_type == 'pump':
-            theta3 += self.alpha
 
-        return {"theta3":theta3, "theta4":theta4, "theta5":theta5, "theta6":theta6} # 有解时返回角度字典
+        return {"theta3":theta3, "theta4":theta4, "theta5":theta5, "theta6":theta6} 
             
 if __name__ == '__main__':
-    ik = IK('arm')
+    ik = IK()
     ik.setLinkLength(L1=ik.l1 + 0.89, L4=ik.l4 - 0.3)
-    print('连杆长度：', ik.getLinkLength())
+    print('Link lengths: ', ik.getLinkLength())
     # print(ik.getRotationAngle((0, 0, ik.l1 + ik.l2 + ik.l3 + ik.l4), 90))
-    print(ik.getRotationAngle((0, 10, 10), 30))
+    print(ik.getRotationAngle((0, 15, 15), 18))
